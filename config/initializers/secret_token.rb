495c208b0c3ee21aa9607b26a79cb9ab7cb1e399
jonesbb <jonesbb@gmail.com>
2012-02-12 14:38:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningJava::Application.config.secret_token = '3ab0f341329bb132ca85854f8298d1c34117a44135e32740568cb542f2a5ec32c42ecc415ec0a9c04608a0999a8df36cac51ec1a2b71083d7fe61bad2b2ecb8a'
