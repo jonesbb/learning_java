8fd8dbeb19efe61a8f8f7566c414c59fe5f64a29
jonesbb <jonesbb@gmail.com>
2012-02-12 15:30:59 -0800

    Done with basic page setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def blog
+    @title = "Blog"
   end
 
   def flightprojects
+    @title = "Flight Projects"
   end
 
   def hackings
+    @title = "Hackings"
   end
 
 end
